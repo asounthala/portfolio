22980939b7e2136156415650e453d0b443f77650
Adrian <adrianxxmail@gmail.com>
2025-05-03 02:51:21 -0500

    Implemeted layout for desktop view

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,21 +3,21 @@ import Navbar from '../ui/navbar'
 
 const Home = () => {
   return (
-    <div className='font-satoshi'>
+    <div className='font-satoshi md:max-w-[900px] mx-auto'>
       <Navbar />
-
-      <div className='flex flex-col justify-center pt-5 pl-10'>
-        <h2 className='text-4xl font-playwrite'> hi there!</h2>
-        <h1 className='text-5xl font-satoshi-black'>i'm <span className='text-emerald-700' >Adrian</span>.</h1>
-        <p className='text-lg pt-3 w-2/3'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        </p>
+      <div className='flex flex-col md:flex-row md:justify-between md:mt-35'>
+        <div className='flex flex-col justify-center pt-5 px-10'>
+          <h2 className='text-3xl md:text-4xl font-playwrite'> hi there!</h2>
+          <h1 className='text-5xl md:text-6xl font-satoshi-black'>i'm <span className='text-emerald-700' >Adrian</span>.</h1>
+          <p className='text-lg md:text-xl pt-3 w-2/3'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className='flex justify-center pt-15'>
+            <img src="src/assets/images/portrait.png" alt="arrow" className='w-64 md:w-[500px]'/>
+        </div>
       </div>
-      <div className='flex justify-center pt-15'>
-          <img src="src/assets/images/portrait.png" alt="arrow" className='w-64'/>
-      </div>
-
     </div>
   )
 }
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HamburgerMenu from './HamburgerMenu'
 
 const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 750)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 750)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
     <nav className='flex justify-between p-8 px-9'>
 
       <a href="/">
-        <img src="src/assets/icons/logo.svg" alt="logo" className='w-10 h-10' />
+        <img src="src/assets/icons/logo.svg" alt="logo" className='w-10 h-10 '/>
       </a>
 
-      <HamburgerMenu />
-
+      {isMobile ? <HamburgerMenu /> : 
+        <div className='font-satoshi-bold text-xl text-neutral-900 '>
+          <ul className='flex space-x-2 '>
+            <li className='inline-block px-4 py-2 hover:underline'>
+              <a href="/">adrian</a>
+            </li>
+            <li className='inline-block px-4 py-2 hover:underline'>
+              <a href="/about">projects</a>
+            </li>
+            <li className='inline-block px-4 py-2 hover:underline'>
+              <a href="/services">about</a>
+            </li>
+            <li className='inline-block px-4 py-2 hover:underline'>
+              <a href="/contact">contact</a>
+            </li>
+          </ul>
+        </div>
+      }
     </nav>
   )
 }
