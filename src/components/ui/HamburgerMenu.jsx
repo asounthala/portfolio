eb2c3f63d8aaad74865f04f4d3e7515e9b763b10
Adrian <adrianxxmail@gmail.com>
2025-05-02 23:29:35 -0500

    Create component for hamburger menu

---
+++ src/components/ui/HamburgerMenu.jsx
@@ -0,0 +1,92 @@
+import Hamburger from 'hamburger-react'
+import React from 'react'
+import { useState, useEffect, useRef } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const HamburgerMenu = () => {
+    const menu = useRef(null)
+    const [isOpen, setIsOpen] = useState(false)
+
+    // Disable scroll when the menu is open
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = 'auto'
+        }
+
+        return () => {
+            document.body.style.overflow = 'auto'
+        }
+    }, [isOpen])
+
+    // Sets the initial position of the menu to be off screen
+    useEffect(() => {
+        if (menu.current) {
+            gsap.set(menu.current, {
+                y: -menu.current.offsetHeight, // used the actual heigh of menu
+            });
+        }
+    }, []);
+
+    // Animate the menu
+    useGSAP(() => {
+        if (menu.current) {
+            if (isOpen) {
+                gsap.fromTo(
+                    menu.current,
+                    { y: -menu.current.offsetHeight },
+                    {
+                        y: 0, duration: 1,
+                        ease: 'power3.out'
+                    }
+                );
+            } else {
+                gsap.to(menu.current,
+                    {
+                        y: -menu.current.offsetHeight
+                        , duration: 0.5
+                    }
+                );
+            }
+        }
+    }
+        , [isOpen])
+    return (
+        <div className='z-1'>
+            <Hamburger
+                size={30}
+                toggled={isOpen}
+                toggle={setIsOpen}
+                color={isOpen ? '#fff' : '#000'}
+                duration={0.3}
+                direction='right'
+                easing='ease-in'
+            />
+
+            <div
+                ref={menu}
+                className={`-z-1 y-w fixed top-0 left-0 w-screen h-screen text-white bg-neutral-900`}
+            >
+                <ul className='h-screen flex flex-col items-center justify-start pt-55 gap-10 text-4xl font-satoshi-bold'>
+                    <li>
+                        <a href="/" className='text-netural-50 hover:text-emerald-700'>&#123; adrian &#125;</a>
+                    </li>
+                    <li>
+                        <a href="/projects" className='text-netural-50 hover:text-emerald-700'>&#123; projects &#125;</a>
+                    </li>
+                    <li>
+                        <a href="/about" className='text-netural-50 hover:text-emerald-700'>&#123; about &#125;</a>
+                    </li>
+                    <li>
+                        <a href="/contact" className='text-netural-50 hover:text-emerald-700'>&#123; contact &#125;</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    )
+}
+
+export default HamburgerMenu
