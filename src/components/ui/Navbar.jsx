0a6443294f3053092d369d969e2c0c88270fa9c7
Adrian <adrianxxmail@gmail.com>
2025-05-05 03:50:46 -0500

    Implement active link styling

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import HamburgerMenu from './HamburgerMenu'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 750)
@@ -20,24 +20,25 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between p-8 px-9 md:max-w-[1000px] mx-auto'>
 
-      <Link to="/">
+      <NavLink to="/">
         <img src="src/assets/icons/logo.svg" alt="logo" className='w-12 h-12 md:w-15 md:h-15 '/>
-      </Link>
+      </NavLink>
 
       {isMobile ? <HamburgerMenu /> : 
         <div className='font-satoshi-medium text-2xl text-slate-900 '>
           <ul className='flex space-x-2 '>
             <li className='inline-block px-4 py-2 hover:underline'>
-              <Link to="/">adrian</Link>
+              <NavLink to="/" className={({ isActive }) => 
+              isActive ? 'opacity-50' : null}>adrian</NavLink>
             </li>
             <li className='inline-block px-4 py-2 hover:underline'>
-              <Link to="/projects">projects</Link>
+              <NavLink to="/projects" className={({ isActive }) =>      isActive ? 'opacity-50' : null}>projects</NavLink>
             </li>
             <li className='inline-block px-4 py-2 hover:underline'>
-              <Link to="/about">about</Link>
+              <NavLink to="/about" className={({ isActive }) => isActive ? 'opacity-50' : null}>about</NavLink>
             </li>
             <li className='inline-block px-4 py-2 hover:underline'>
-              <Link to="/contact">contact</Link>
+              <NavLink to="/contact">contact</NavLink>
             </li>
           </ul>
         </div>
