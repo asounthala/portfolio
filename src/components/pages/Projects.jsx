4c1fb9166d992b6a4b46ab9dae8b4c56f6089ec5
Adrian <adrianxxmail@gmail.com>
2025-05-12 02:57:31 -0500

    Create project card componenet

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import Navbar from '../ui/navbar'
+import ProjectCard from '../ui/ProjectCard'
 
 const Projects = () => {
   return (
     <>
       <Navbar />
       <div className='font-satoshi md:max-w-[900px] mx-auto'>
-          <h1 className='flex justify-center font-satoshi-black text-6xl'>projects</h1>
-          <p className='flex justify center text-center text-lg pt-6 w-3/4 mx-auto text-neutral-600'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+        <h1 className='flex justify-center font-satoshi-black text-6xl'>projects</h1>
+        <p className='flex justify center text-center text-lg pt-6 w-3/4 mx-auto text-neutral-600'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className='flex justify-center'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+        gap-4 p-10'>
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
+          <ProjectCard />
         </div>
+      </div>
     </>
   )
 }
---
+++ src/components/ui/ProjectCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProjectCard = () => {
+  return (
+    <div className='bg-gray-200 w-80 h-80 '>
+        <h1 className='text-xl font-satoshi-medium pt-4 pl-4'>
+            project name
+        </h1>
+    </div>
+  )
+}
+
+export default ProjectCard
