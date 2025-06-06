08d978bfa6c49af8718bbccd518040d640738442
Adrian <adrianxxmail@gmail.com>
2025-06-06 04:22:34 -0500

    Add more padding to summary

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
           <div className='flex flex-col justify-center pt-5 px-5'>
             <h2 className='text-3xl md:text-4xl font-playwrite'> hi there!</h2>
             <h1 className='text-5xl md:text-6xl font-satoshi-black'>i'm <span className='text-emerald-700' >Adrian</span>.</h1>
-            <p className='text-lg md:text-xl pt-3 md:w-3/4 text-neutral-600'>
+            <p className='text-lg md:text-xl pt-6 md:w-3/4 text-neutral-600'>
               I'm a recent Computer Science graduate from the University of Minnesota Twin Cities. 
               I'm a curious and creative developer with a passion for full-stack development and building clean, useful software.
             </p>
@@ -20,10 +20,6 @@ const Home = () => {
             <img src="src/assets/images/portrait.jpg" alt="self portrait" className='w-64 md:w-[1000px] border-zinc-950 border-4' />
           </div>
         </div>
-        <Link to="/projects"
-          className='text-neutral-600 hover:underline text-lg md:text-xl absolute bottom-7 right-5'>
-          &#123; go to projects  &#125;
-        </Link>
       </div>
     </>
   )
