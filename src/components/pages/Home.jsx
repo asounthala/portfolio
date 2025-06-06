7b9c721b2894cff22c300d4ec016bd717976dc31
Adrian <adrianxxmail@gmail.com>
2025-06-06 03:46:01 -0500

    Update home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,13 +11,13 @@ const Home = () => {
           <div className='flex flex-col justify-center pt-5 px-5'>
             <h2 className='text-3xl md:text-4xl font-playwrite'> hi there!</h2>
             <h1 className='text-5xl md:text-6xl font-satoshi-black'>i'm <span className='text-emerald-700' >Adrian</span>.</h1>
-            <p className='text-lg md:text-xl pt-3 md:w-2/3 text-neutral-600'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <p className='text-lg md:text-xl pt-3 md:w-3/4 text-neutral-600'>
+              I'm a recent Computer Science graduate from the University of Minnesota Twin Cities. 
+              I'm a curious and creative developer with a passion for full-stack development and building clean, useful software.
             </p>
           </div>
           <div className='flex justify-center pt-10 md:pr-15'>
-            <img src="src/assets/images/portrait.png" alt="self portrait" className='w-64 md:w-[500px]' />
+            <img src="src/assets/images/portrait.jpg" alt="self portrait" className='w-64 md:w-[1000px] border-zinc-950 border-4' />
           </div>
         </div>
         <Link to="/projects"
