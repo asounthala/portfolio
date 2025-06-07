40804d20b4bc343df4f6de070dc48d326456782c
Adrian <adrianxxmail@gmail.com>
2025-06-07 03:17:03 -0500

    Changed styling to projects page and projectcard

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -8,17 +8,12 @@ const Projects = () => {
       <Navbar />
       <div className='font-satoshi md:max-w-[900px] mx-auto'>
         <h1 className='flex justify-center font-satoshi-black text-6xl'>projects</h1>
-        <p className='flex justify center text-center text-lg pt-6 w-3/4 mx-auto text-neutral-600'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <p className='flex justify-center text-center text-lg pt-6 w-3/4 mx-auto text-neutral-600'>
+          Check out some cool projects I've worked on!
         </p>
       </div>
       <div className='flex justify-center'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
-        gap-4 p-10'>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+        <div className='grid grid-cols-1 gap-8 p-10'>
           <ProjectCard />
           <ProjectCard />
           <ProjectCard />
--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ProjectCard = () => {
   return (
-    <div className='bg-gray-200 w-80 h-80 '>
-        <h1 className='text-xl font-satoshi-medium pt-4 pl-4'>
+    <div className='bg-gray-300 w-80 md:w-200 h-80 '>
+        <h1 className='text-3xl font-satoshi-medium pt-7 pl-7'>
             project name
         </h1>
     </div>
