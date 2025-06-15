05229becb9842e117a69daf2d847341f03472b91
Adrian <adrianxxmail@gmail.com>
2025-06-15 01:51:21 -0500

    Fix bg image for card, add more styling

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -14,9 +14,12 @@ const Projects = () => {
       </div>
       <div className='flex justify-center'>
         <div className='grid grid-cols-1 gap-8 p-10'>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          <ProjectCard 
+            name="Project One"
+            description="This is a description of project one. It showcases the features and functionalities of the project."
+            image="src/assets/images/dog.jpg"
+            link="https://www.youtube.com/watch?v=hJresi7z_YM?&autoplay=1"
+          />
         </div>
       </div>
     </>
--- src/components/ui/ProjectCard.jsx
+++ src/components/ui/ProjectCard.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = ({ name, description, image, link }) => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 750)
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 750)
+      }
+  
+      window.addEventListener('resize', handleResize)
+  
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    }, [])
   return (
-    <div className='bg-gray-300 w-80 md:w-200 h-80 '>
-        <h1 className='text-3xl font-satoshi-medium pt-7 pl-7'>
-            project name
+    <div 
+      className='relative md:bg-gray-300 w-80 md:w-200 h-80 bg-cover bg-center cursor-pointer'
+      style={{ backgroundImage: image ? `url(${image})` : undefined}}
+      onClick={() => {
+        if (link) {
+          window.open(link, '_blank')
+        }
+      }}
+    >
+      {image && (
+      <div className="absolute inset-0 bg-black opacity-40" />
+      )}
+      <div className='relative z-10 h-full'>
+        <h1 className='text-4xl font-satoshi-medium pt-7 pl-7 text-white'>
+          {name || 'Project Name'}
         </h1>
+        <p className='absolute bottom-10 text-xl font-satoshi-light pt-5 pl-7 pr-7 text-white'>
+          {description || 'This is a brief description of the project. It highlights the main features and functionalities.'}
+        </p>
+      </div>
     </div>
   )
 }
